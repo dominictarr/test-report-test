e5203b0ea014e3a6242a91cde1ee83cba15c9d81
Dominic Tarr <dominic.tarr@gmail.com>
2011-07-28 17:21:23 +1000

    added test for failure count (the number of errors thrown)
    (not exactly the same as the number of failing tests -- since a test can generate multiple errors and tests can be nested, giving a number of tests failed is not straightforward. but giving the number of errors is)

---
+++ failCount.node.js
@@ -0,0 +1,37 @@
+var it = require('it-is')
+  , Report = require('test-report')
+  , test = module.exports
+  , assert = require('assert')
+
+;(function(){
+
+  var r = Report(__filename)
+    , err = new Error("EXAMPLE ERROR")
+    , err2 = new Error("EXAMPLE ERROR")
+
+  r.test('error', err)
+  r.test('error', err2)
+
+  it(r.report).has({
+    failureCount: 2
+  , tests: [{
+      failureCount: 2
+    }]
+  })
+
+})()
+
+;(function(){
+
+  var r = Report(__filename)
+
+  r.test('pass')
+
+  it(r.report).has({
+    failureCount: 0
+  , tests: [{
+      failureCount: 0
+    }]
+  })
+
+})()
